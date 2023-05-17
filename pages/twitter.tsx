b0aef47a8d7393fc110850d68e1663b140fcafe8
ytorium <ytorium@gmail.com>
2023-05-17 16:46:44 +0300

    Commit 1

--- pages/twitter.tsx
+++ pages/twitter.tsx
@@ -1,3 +1,4 @@
+import Layout from "../components/layout"
 import TwitterApi from "twitter-api-v2"
 
 export default function Twitter() {
@@ -5,13 +6,17 @@ export default function Twitter() {
   console.log("user1");
   const twitterClient = new TwitterApi('6lzVZHTWWEaqocAewMyhNFdmL');
   const readOnlyClient = twitterClient.readOnly;
-  const user = await readOnlyClient.v2.userByUsername('YToimich');
-  console.log(user);
+  //async function example() {
+  //  const user = await readOnlyClient.v2.userByUsername('YToimich');
+  //  console.log(user);
+  //}
   console.log("user2");
 
   return (
+    <Layout>
       <div>
        <p>Hello Mir</p>
       </div>
+    </Layout>
   )
 }
