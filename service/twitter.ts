1c15743fe86fa5828a5d61f96178bfb2dfddffe7
ytorium <ytorium@gmail.com>
2023-05-18 19:57:14 +0300

    Commit 8

--- service/twitter.ts
+++ service/twitter.ts
@@ -2,9 +2,15 @@ import TwitterApi from "twitter-api-v2"
 
 export const fetchUser = async (user_name : any) => {
   console.log("user1");
-  const twitterClient = new TwitterApi('6lzVZHTWWEaqocAewMyhNFdmL');
-  const readOnlyClient = twitterClient.readOnly;
-  const userInfo = await readOnlyClient.v2.userByUsername(user_name);
+  const twitterClient = new TwitterApi({
+    appKey: process.env.APP_KEY!,
+    appSecret: process.env.APP_SECRET!,
+    accessToken: process.env.ACCESS_TOKEN!,
+    accessSecret: process.env.ACCESS_SECRET!
+  });
+  //const twitterClient = new TwitterApi('6lzVZHTWWEaqocAewMyhNFdmL');
+  //const readOnlyClient = twitterClient.readOnly;
+  const userInfo = await twitterClient.v2.userByUsername(user_name);
   console.log(userInfo);
   console.log("user2");
   return userInfo;
